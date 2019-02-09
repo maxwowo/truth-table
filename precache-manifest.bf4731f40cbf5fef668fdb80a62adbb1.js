a83cc5424c2cc07ce6ed53d16f0819fd20df42bb
Max Wo <maxwo@protonmail.com>
2019-02-10 00:50:17 +1100

    Updates

---
+++ precache-manifest.bf4731f40cbf5fef668fdb80a62adbb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff4d052d96634ca96f83",
+    "url": "/truth-table-generator/static/js/runtime~main.ff4d052d.js"
+  },
+  {
+    "revision": "044ffe048e76bd285cb4",
+    "url": "/truth-table-generator/static/js/main.044ffe04.chunk.js"
+  },
+  {
+    "revision": "5ebdbeddbfda52134280",
+    "url": "/truth-table-generator/static/js/1.5ebdbedd.chunk.js"
+  },
+  {
+    "revision": "044ffe048e76bd285cb4",
+    "url": "/truth-table-generator/static/css/main.137fa407.chunk.css"
+  },
+  {
+    "revision": "5ebdbeddbfda52134280",
+    "url": "/truth-table-generator/static/css/1.ce76bdff.chunk.css"
+  },
+  {
+    "revision": "90acea2e671b5fc19fbd240d48be50e0",
+    "url": "/truth-table-generator/index.html"
+  }
+];
